0737105a00a7f9a958c8c19b41a7576e45c0984c
Andrew Marshall <amarshall@bignerdranch.com>
2019-06-06 12:33:49 -0500

    Set up fragment view

--- app/src/main/java/com/bignerdranch/cameraxmlkitblog/CameraFragment.kt
+++ app/src/main/java/com/bignerdranch/cameraxmlkitblog/CameraFragment.kt
@@ -1,6 +1,21 @@
 package com.bignerdranch.cameraxmlkitblog
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.TextureView
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 
 class CameraFragment : Fragment() {
+
+  private lateinit var cameraView: TextureView
+
+  override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+    val view = inflater.inflate(R.layout.fragment_camera, container, false)
+
+    cameraView = view.findViewById(R.id.camera_view)
+
+    return view
+  }
 }
