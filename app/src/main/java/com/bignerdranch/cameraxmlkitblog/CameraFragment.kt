f778e824f31f7afa11cb02f14b217b4b16fc1fbc
Andrew Marshall <amarshall@bignerdranch.com>
2019-06-06 12:56:40 -0500

    Add cameraX

--- app/src/main/java/com/bignerdranch/cameraxmlkitblog/CameraFragment.kt
+++ app/src/main/java/com/bignerdranch/cameraxmlkitblog/CameraFragment.kt
@@ -1,10 +1,14 @@
 package com.bignerdranch.cameraxmlkitblog
 
 import android.os.Bundle
+import android.util.DisplayMetrics
+import android.util.Rational
+import android.util.Size
 import android.view.LayoutInflater
 import android.view.TextureView
 import android.view.View
 import android.view.ViewGroup
+import androidx.camera.core.CameraX
 import androidx.fragment.app.Fragment
 
 class CameraFragment : Fragment() {
@@ -16,6 +20,19 @@ class CameraFragment : Fragment() {
 
     cameraView = view.findViewById(R.id.camera_view)
 
+    cameraView.post {
+      setUpCameraX()
+    }
+
     return view
+  }
+
+  private fun setUpCameraX() {
+    CameraX.unbindAll()
+
+    val displayMetrics = DisplayMetrics().also { cameraView.display.getRealMetrics(it) }
+    val screenSize = Size(displayMetrics.widthPixels, displayMetrics.heightPixels)
+    val aspectRatio = Rational(displayMetrics.widthPixels, displayMetrics.heightPixels)
+    val rotation = cameraView.display.rotation
   }
 }
